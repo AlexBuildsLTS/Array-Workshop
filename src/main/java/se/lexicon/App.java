8120d72871b1634e04c9e7ec6af3045eb693324f
FullStackOS <alex.youssef@live.com>
2024-06-19 11:07:40 +0200

    Implemented part 1 methods

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -5,9 +5,16 @@ public class App {
     public static void main(String[] args) {
 
         int size = NameRepository.getSize();
-        System.out.println(size);
+        System.out.println("Initial size: " + size);
+
         NameRepository.setNames(new String[]{"Erik Svensson", "Mehrdad Javan"});
-        System.out.println(NameRepository.getSize());
-        // call more methods as needed
+        size = NameRepository.getSize();
+        System.out.println("Size after setting names: " + size);
+
+
+        String[] allNames = NameRepository.findAll();
+        System.out.println("All names: " + String.join(", ", allNames)); // Expected output: Erik Svensson, Mehrdad Javan
+
+
     }
 }
